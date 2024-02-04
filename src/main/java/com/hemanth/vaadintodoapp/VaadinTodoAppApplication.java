b2bf214d6f4cf80ce592fc3aa3dc5ec788d43ee4
hemanthcse1 <hemanthcse1@gmail.com>
2024-02-04 22:16:51 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/vaadintodoapp/VaadinTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.vaadintodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hemanth/vaadintodoapp/VaadinTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.vaadintodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
